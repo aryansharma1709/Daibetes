4316f601650f65de1bf2bd64ed2d634ed2ee15d4
aryansharma <aryansharmag29@gmail.com>
2024-11-28 01:32:41 +0530

    update

--- daibetes_streamlit.py
+++ daibetes_streamlit.py
@@ -1,11 +1,8 @@
 import streamlit as st
 import pandas as pd
 import joblib
-import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
-import seaborn as sns
-
 
 
 # Set page configuration
